230f965838a424496ab0b3e0c99cbacfde5fda82
Nookaraju Maddila <Nookaraju.Maddila@infor.com>
2017-07-31 11:41:34 +0530

    Added Files

---
+++ MyTestPod/Classes/CircularProgressView.h
@@ -0,0 +1,26 @@
+//
+//  CircularProgressView.h
+//  IDM
+//
+//  Created by Nookaraju Maddila on 3/7/17.
+//  Copyright © 2017 Infor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CircularProgressView : UIView
+@property(nonatomic, strong) UIColor *trackTintColor UI_APPEARANCE_SELECTOR;
+@property(nonatomic, strong) UIColor *progressTintColor UI_APPEARANCE_SELECTOR;
+@property(nonatomic, strong) UIColor *innerTintColor UI_APPEARANCE_SELECTOR;
+@property(nonatomic) NSInteger roundedCorners UI_APPEARANCE_SELECTOR; // Can not use BOOL with UI_APPEARANCE_SELECTOR :-(
+@property(nonatomic) CGFloat thicknessRatio UI_APPEARANCE_SELECTOR;
+@property(nonatomic) NSInteger clockwiseProgress UI_APPEARANCE_SELECTOR; // Can not use BOOL with UI_APPEARANCE_SELECTOR :-(
+@property(nonatomic) CGFloat progress;
+
+@property(nonatomic) CGFloat indeterminateDuration UI_APPEARANCE_SELECTOR;
+@property(nonatomic) NSInteger indeterminate UI_APPEARANCE_SELECTOR; // Can not use BOOL with UI_APPEARANCE_SELECTOR :-(
+
+- (void)setProgress:(CGFloat)progress animated:(BOOL)animated;
+- (void)setProgress:(CGFloat)progress animated:(BOOL)animated initialDelay:(CFTimeInterval)initialDelay;
+- (void)setProgress:(CGFloat)progress animated:(BOOL)animated initialDelay:(CFTimeInterval)initialDelay withDuration:(CFTimeInterval)duration;
+@end
